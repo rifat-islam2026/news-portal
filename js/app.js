212f0e1b6c87e29eee9c607790404e259cc399ce
rifat-islam2026 <rifat.info001@gmail.com>
2024-05-21 01:53:48 +0600

    simple finish

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@
         displayCategoryItem(data.data.news_category);
     };
     // display Category
+    const categoryContainer = document.getElementById('category-container');
     const displayCategoryItem = (items) => {
-        const categoryContainer = document.getElementById('category-container');
         items.forEach(categoryItem => {
             // console.log(categoryItem.category_name);
             const categoryDiv = document.createElement('div');
@@ -51,12 +51,12 @@ const displayNews = (news) => {
                        <div class="ms-2"> <h6>${post.author.name}</h6> <small class="text-secondary">${post.author.published_date}</small> </div>
                       </div>
 
-                         <div class="total-view"><div class="d-flex align-items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                         <div class="total-view text-secondary"><div class="d-flex align-items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                         <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
                          <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
                         </svg> <span class="ms-2">${post.total_view ? post.total_view : 'No data found!'}</span> </div></div>
 
-                    <div class="star" id="news-details">
+                    <div onclick="loadNewsDetails('${post._id}')" class="star text-primary" id="news-details" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">
                     <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
                     </svg>
@@ -83,6 +83,43 @@ const toggleSpinner = isLoading => {
         spinner.classList.add('d-none');
     }
 };
+// load news details 
+const loadNewsDetails = async (news_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsDetails(data.data[0]);
+};
 
-loadCategoryItem(); 
+// display news details 
+const displayNewsDetails = (post) => {
+    const postName = document.getElementById('post-name');
+    postName.innerText = `${post.author ? post.author.name : 'No data found!'}`;
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = `
+    <h6>Title: ${post.title}</h6>
+    <p>Published Date: ${post.author.published_date}</p>
+    `;
+
+};
+// Blog / Question
+document.getElementById('blog').addEventListener('click', function () {
+    categoryContainer.innerHTML = '';
+    toggleBlog(true);
+});
+document.getElementById('news').addEventListener('click', function () {
+    loadCategoryItem();
+    toggleBlog(false);
+});
+const toggleBlog = isLoading => {
+    const questions = document.getElementById('questions');
+    if (isLoading) {
+        questions.classList.remove('d-none');
+    }
+    else {
+        questions.classList.add('d-none');
+    }
+};
+
+ 
     
